daed9674329c86efe73d3051827eec537dbd3306
9gr <67440879+FunctionalMetatable@users.noreply.github.com>
2021-03-31 10:14:21 +0100

    Push new changes

--- script.js
+++ script.js
@@ -5,22 +5,38 @@ var apps = [
     name: "ScratchSuite",
     repository: "scratchsuite/scratchsuite.github.io",
     owner: "ScratchSuite",
-    url: "//scratchsuite.github.io"
+    url: "//scratchsuite.github.io",
+    tags: ["beta"]
   },
   {
     name: "Frontend",
     repository: "scratchsuite/frontend",
     owner: "ScratchSuite",
-    url: "//scratchsuite.github.io/frontend"
+    url: "//scratchsuite.github.io/frontend",
+    tags: ["beta"]
   },
   {
     name: "Backend",
     repository: "scratchsuite/Backend",
-    owner: "ScratchSuite",
-    url: "//scratchsuite.github.io/backend"
+    owner: "FunctionalMetatable",
+    url: "//scratchsuite.github.io/backend",
+    tags: ["beta", "upptime"]
+  },
+  {
+    name: "Scratch 1.0 Forums",
+    repository: "scratchsuite/Backend",
+    owner: "Andrew Sun",
+    url: "//scratcharchive.asun.co/forums/",
+    tags: ["Forums", "Old"],
+    ownerPage: "//asun.co"
   },
 ];
 
+var tagSettings = {
+  beta: {
+    color: "#ffa500"
+  }
+}
 for (let i=0;i<apps.length;i++) {
   var app = apps[i];
   // Create the div
@@ -31,8 +47,33 @@ for (let i=0;i<apps.length;i++) {
   <h4>${app.name}</h4>
   <a>by <a href="${app.ownerPage || "https://scratchsuite.github.io"}">${app.owner}</a></a>
   </a>
+  <br>
   </div>
   `
 
   div.innerHTML += data
+
+  app.div = document.getElementById(`app-${app.name}`)
+  // Add the tags
+
+  for (let tagI=0;tagI<app.tags.length;tagI++) {
+    var tag = document.createElement("span");
+    tag.setAttribute("class", "tag")
+    tag.style.borderColor = "#FFF"
+    tag.innerText = app.tags[tagI]
+    var settings = tagSettings[app.tags[tagI]]
+    if (settings && settings.color) tag.style.backgroundColor = settings.color
+    
+    app.div.appendChild(tag)
+    if (app.tags.length >=2) app.div.innerHTML += "⠀⠀"
+  }
+
+  // create <br> el
+  app.div.appendChild(document.createElement("br"))
+
+  // create an anchor element for the br to work
+  ph=document.createElement("p");
+  ph.innerText="​"
+  ph.setAttribute("class", "app-div-whitespace-border")
+  app.div.appendChild(ph)
 }
